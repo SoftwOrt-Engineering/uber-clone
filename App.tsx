d78eabcc5d367e38865933f34e3be7a6a4503205
Markus Ort <ort@singhammer.com>
2021-09-26 20:20:04 +0200

    added react-native-elements

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 // Redux Tolkit
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+// Helpers
+import { SafeAreaProvider } from "react-native-safe-area-context";
 // Native Comps
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
@@ -11,8 +13,10 @@ import { HomeScreen } from "./src/screens";
 export default function App() {
   return (
     <Provider store={store}>
-      <StatusBar style="auto" />
-      <HomeScreen />
+      <SafeAreaProvider>
+        <StatusBar style="auto" />
+        <HomeScreen />
+      </SafeAreaProvider>
     </Provider>
   );
 }
