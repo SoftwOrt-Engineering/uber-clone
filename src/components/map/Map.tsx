fec6a5231b333bca9ac84c71c4d436f833edbcfe
Markus Ort <ort@singhammer.com>
2021-09-30 20:52:09 +0200

    impl. Marker for Map

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -11,21 +11,30 @@ export const Map: FunctionComponent = () => {
   const origin = useAppSelector(getOrigin);
 
   return (
-    <View>
-      <MapView
-        style={styles.mapView}
-        mapType="mutedStandard"
-        initialRegion={{
-          latitude: origin && origin.location ? origin.location.lat : 48.137154,
-          longitude:
-            origin && origin.location ? origin.location.lng : 11.576124,
-          latitudeDelta:
-            origin && origin.location && origin.location.lng ? 0.005 : 0.07,
-          longitudeDelta:
-            origin && origin.location && origin.location.lng ? 0.005 : 0.07,
-        }}
-      />
-    </View>
+    <MapView
+      style={styles.mapView}
+      mapType="mutedStandard"
+      initialRegion={{
+        latitude: origin && origin.location ? origin.location.lat : 48.137154,
+        longitude: origin && origin.location ? origin.location.lng : 11.576124,
+        latitudeDelta:
+          origin && origin.location && origin.location.lng ? 0.005 : 0.07,
+        longitudeDelta:
+          origin && origin.location && origin.location.lng ? 0.005 : 0.07,
+      }}
+    >
+      {origin && origin.location && (
+        <Marker
+          coordinate={{
+            latitude: origin.location.lat,
+            longitude: origin.location.lng,
+          }}
+          title="Origin"
+          description={origin.description ? origin.description : ""}
+          identifier="origin"
+        />
+      )}
+    </MapView>
   );
 };
 
